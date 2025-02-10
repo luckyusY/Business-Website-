01fa36a62d4331c0e09951192f7375cbf39ed9ed
Yahya <nkundimanarugirayahaya@gmail.com>
2025-02-10 04:56:57 +0200

    message

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html", // Add paths to your HTML files
+      "./about.html",
+      "./contact.html",
+      "./services.html",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
